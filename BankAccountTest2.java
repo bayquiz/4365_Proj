897aaac3ce2bebb3c796b33c320811a48f2e79fe
charbel1998 <60449346+charbel1998@users.noreply.github.com>
2020-04-04 15:26:21 -0500

    Add files via upload

---
+++ BankAccountTest2.java
@@ -0,0 +1,34 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BankAccountTest2 {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		BankAccount b= new BankAccount();
+		double Expected= 24.0;
+		double test= b.deposit();
+		System.out.println("Cannot test this method with Junit because this methode requires User input");
+	}
+}
